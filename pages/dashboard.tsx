890f4b85be72e108fd7f0216781633730f33b80e
Tengku Hafidz <tengkuhafidz@gmail.com>
2022-09-25 16:05:34 +0800

    fix(auth): bad import

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,9 +1,7 @@
+import { withPageAuth } from "@supabase/auth-helpers-nextjs";
 import { AppShell } from "../ui/AppShell";
 
 export const getServerSideProps = withPageAuth({ redirectTo: "/login" });
-function withPageAuth(arg0: { redirectTo: string }) {
-  throw new Error("Function not implemented.");
-}
 
 const Dashboard = () => {
   return <AppShell>Dashboard</AppShell>;
