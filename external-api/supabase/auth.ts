7504db1bd20e2b3e0b5f3ecac84caa6729ad0dd5
Tengku Hafidz <tengkuhafidz@gmail.com>
2022-10-01 13:17:44 +0800

    feat(auth): test login to subdomain

--- external-api/supabase/auth.ts
+++ external-api/supabase/auth.ts
@@ -3,7 +3,7 @@ import { supabaseClient } from '@supabase/auth-helpers-nextjs'
 
 
 const handleSignIn = async (email: string) => {
-  return await supabase.auth.signIn({email}, {shouldCreateUser: false, redirectTo: "https://mmo.sg"});
+  return await supabase.auth.signIn({email}, {shouldCreateUser: false, redirectTo: "https://go.mmo.sg"});
 }
 
 const handleSignOut = async () => {
