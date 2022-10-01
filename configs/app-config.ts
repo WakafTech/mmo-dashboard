e24031b718c79bbb243c815902849a9464ef855a
Tengku Hafidz <tengkuhafidz@gmail.com>
2022-10-01 18:00:44 +0800

    fix(auth): get redirect url from env

--- configs/app-config.ts
+++ configs/app-config.ts
@@ -1,4 +1,7 @@
 export const clientConfig = {
+  app: {
+    baseUrl: process.env.NEXT_PUBLIC_BASE_URL!
+  },
   supabase: {
     url: process.env.NEXT_PUBLIC_SUPABASE_URL!,
     anonKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
--- external-api/supabase/auth.ts
+++ external-api/supabase/auth.ts
@@ -1,16 +1,19 @@
 import supabase from "./client";
-import { supabaseClient } from '@supabase/auth-helpers-nextjs'
-
+import { supabaseClient } from "@supabase/auth-helpers-nextjs";
+import { clientConfig } from "../../configs/app-config";
 
 const handleSignIn = async (email: string) => {
-  return await supabase.auth.signIn({email}, {shouldCreateUser: false, redirectTo: "https://go.mmo.sg"});
-}
+  return await supabase.auth.signIn(
+    { email },
+    { shouldCreateUser: false, redirectTo: clientConfig.app.baseUrl }
+  );
+};
 
 const handleSignOut = async () => {
-  return await supabaseClient.auth.signOut()
-}
+  return await supabaseClient.auth.signOut();
+};
 
 export const Auth = {
   handleSignIn,
   handleSignOut,
-}
+};
